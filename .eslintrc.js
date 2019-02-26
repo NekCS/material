ef98a779e24d769ceb372135203d70d8eb2234ac
Nektarios <nek.liakis@gmail.com>
2019-02-26 09:51:19 +0200

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "eslint:recommended",
+    "env": {
+        "es6": true,
+        "browser": true,
+        "jquery": true
+    },
+    "globals": {
+        "mdc": true
+    },
+    "rules": {
+        "linebreak-style": 0,
+        "indent": ["error", 4]
+    }
+};
